f8bb0e3a5496eb41edb02ded08168f90f982dbe1
RyanHPaez <solosnail@gmail.com>
2022-04-13 16:12:36 -0700

    m

--- models/index.js
+++ models/index.js
@@ -3,21 +3,21 @@ const mongoose = require('mongoose')
 
 const db = process.env.MONGO || 'test'    
     
-// mongoose.connect(db, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology:true,
-//       useCreateIndex: true
-//     }).then(()=>{
-//       console.log("conected to mongodb");
-//     }).catch(error => {
-//       console.log("mongo error",error);
-//     })
+mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology:true,
+      useCreateIndex: true
+    }).then(()=>{
+      console.log("conected to mongodb");
+    }).catch(error => {
+      console.log("mongo error",error);
+    })
 
-mongoose.connect(process.env.MONGODB_URI.DB, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true, 
+//   useUnifiedTopology: true,
 
-})
+// })
 
 module.exports.Place = require('./places')
 module.exports.Comment = require('./comment')
