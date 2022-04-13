e0b7405e52445a1ae8fce09fb261f30814cc80bb
RyanHPaez <solosnail@gmail.com>
2022-04-13 15:45:53 -0700

    M

--- models/index.js
+++ models/index.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose')
 
 const db = process.env.MONGO || 'test'    
     
-mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology:true,
-      useCreateIndex: true
-    }).then(()=>{
-      console.log("conected to mongodb");
-    }).catch(error => {
-      console.log("mongo error",error);
-    })
+// mongoose.connect(db, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology:true,
+//       useCreateIndex: true
+//     }).then(()=>{
+//       console.log("conected to mongodb");
+//     }).catch(error => {
+//       console.log("mongo error",error);
+//     })
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
