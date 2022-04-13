7ebcee5c698fe2b04129ef4c605181b334cb31fa
RyanHPaez <solosnail@gmail.com>
2022-04-13 16:43:51 -0700

    it works for now

--- models/index.js
+++ models/index.js
@@ -1,22 +1,19 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const db = process.env.MONGO || 'test'    
+  
+
     
-mongoose.connect(db, {
+mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology:true,
-      useCreateIndex: true
+    
     }).then(()=>{
       console.log("conected to mongodb");
     }).catch(error => {
       console.log("mongo error",error);
     })
 
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true, 
-//   useUnifiedTopology: true,
 
-// })
 
 module.exports.Place = require('./places')
 module.exports.Comment = require('./comment')
