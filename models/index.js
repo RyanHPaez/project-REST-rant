9663e59b73f2392f0d46f52bed502a8274bb957a
RyanHPaez <solosnail@gmail.com>
2022-04-13 15:50:47 -0700

    m

--- models/index.js
+++ models/index.js
@@ -13,11 +13,11 @@ mongoose.connect(db, {
       console.log("mongo error",error);
     })
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true, 
+//   useUnifiedTopology: true,
 
-})
+// })
 
 module.exports.Place = require('./places')
 module.exports.Comment = require('./comment')
