e96853a9f3c04994eda1221554d7961c8475c2b1
ryanhpaez <solosnail@gmail.com>
2022-10-20 19:33:31 -0700

    make it better

--- models/index.js
+++ models/index.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose')
 
     
 mongoose.connect("process.env.MONGO_URI", {
-      useNewUrlParser: true,
-      useUnifiedTopology:true,
+      
     
     }).then(()=>{
       console.log("conected to mongodb");
