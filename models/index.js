fc3ee3d5203f0f0419ba4df058d9b9d24ba175a1
ryanhpaez <solosnail@gmail.com>
2022-10-20 19:42:40 -0700

    make it better

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
     
 mongoose.connect("mongodb+srv://Solosnail:ZsVL6VBScJDTJPw@cluster0.6mr3r.mongodb.net/project-REST-rant", {
-      'useNewUrlParser: true',
+      'useNewUrlParser: true':
       'useUnifiedTopology:true',
     
     }).then(()=>{
