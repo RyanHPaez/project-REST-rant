b97ef59a168b51e6276382b6cf7ad3a71e28a025
ryanhpaez <solosnail@gmail.com>
2022-10-20 19:25:40 -0700

    make it better

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
 require('dotenv').config()
-
+const mongoose = require('mongoose')
   
 
     
-const url = process.env.MONGO_URL
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.createConnection(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology:true,
     
-    .then(()=>{
+    }).then(()=>{
       console.log("conected to mongodb");
     }).catch(error => {
       console.log("mongo error",error);
