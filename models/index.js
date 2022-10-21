094f6c771f11d54e9289ef8a6f04b95c39ba72f6
ryanhpaez <solosnail@gmail.com>
2022-10-21 14:35:30 -0700

    disapear

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
   
 
     
-mongoose.connect("mongodb+srv://Solosnail:ZsVL6VBScJDTJPw@cluster0.6mr3r.mongodb.net/project-REST-rant", {
+mongoose.connect("", {
     
     }).then(()=>{
       console.log("conected to mongodb");
